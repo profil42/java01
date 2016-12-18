dc1ac09b7f122cb382d64a7feaa8d92953c3cda8
a <a@profil-2d81e83c>
2016-12-18 09:00:32 +0100

    java02

--- src/p02/Klasse01.java
+++ src/p02/Klasse01.java
@@ -5,7 +5,7 @@ public class Klasse01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hurra01");
+		System.out.println("Hurra02");
 
 	}
 
